63d8464c316b82d6317b68f1a0726c48eed5c16a
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-05-14 21:45:45 -0400

    Connection to db successful

--- server/db.js
+++ server/db.js
@@ -1,19 +1,23 @@
-const mongoose = require('mongoose');
-require('dotenv').config();
-const mongoUrl = process.env.MONGO_URL || 'mongodb://localhost/my-app';
+import pkg from 'mongoose';
+const { connect, connection } = pkg;
+// change from require to import
+import dotenv from 'dotenv';
+dotenv.config();
 
-mongoose.connect(mongoUrl, {
+//require('dotenv').config();
+const mongoUrl = process.env.MONGO_URL;
+
+const con = connect(mongoUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  useCreateIndex: true,
 }).then(() => {
   console.log('MongoDB connected');
 }).catch((err) => {
   console.error('Error connecting to MongoDB: ', err);
 });
 
-mongoose.connection.on('error', (err) => {
+connection.on('error', (err) => {
   console.error('MongoDB connection error: ', err);
 });
 
-module.exports = mongoose;
+export default con;
--- server/index.js
+++ server/index.js
@@ -1,25 +1,53 @@
-require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const db = require('./db');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import bodyParser from 'body-parser';
+import db from './db.js';
 const app = express();
+import cors from 'cors';
+
+const createUser = async (username, password, email) => {
+  try {
+    const usersCollection = db.collection('users');
+  
+    const userSchema = new db.Schema({
+      username: { type: String, required: true },
+      password: { type: String, required: true },
+      email: { type: String, required: true }
+    });
+    const User = db.model('User', userSchema);
+    
+    const user = new User({
+      username: username,
+      password: password,
+      email: email
+    });
+
+    const result = await usersCollection.insertOne(user);
+    const userId = result.insertedId;
+    return userId;
+  } catch (error) {
+    console.error('Error creating user account: ', error);
+    throw new Error('Internal server error');
+  }
+};
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-app.post('/register', async (req, res) => {
+
+app.post('register', async (req, res) => {
   try {
     const { username, password, email } = req.body;
 
-    // Create a new user account
-    const User = db.model('User', {
-      username: String,
-      password: String,
-      email: String,
+    const userId = await createUser(username, password, email);
+    // send response as a JSON
+    res.json({
+      success: true,
+      message: 'User account created',
+      userId: userId
     });
-    const user = new User({ username, password, email });
-    await user.save();
-
     res.status(201).json({ success: true, message: 'User account created' });
   } catch (err) {
     console.error('Error creating user account: ', err);
