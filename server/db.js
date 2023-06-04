ee5434b36800d83eb874b5e18041629d736299c2
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-06-03 22:03:11 -0400

    update .gitignore

--- server/db.js
+++ server/db.js
@@ -2,7 +2,6 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 
-//require('dotenv').config();
 const mongoUrl = process.env.MONGO_URL;
 
 mongoose.connect(mongoUrl, {
