9bc341151e3005753f0ca46019e630b16c6ac16b
hz-zh <115835509+hz-zh@users.noreply.github.com>
2023-07-02 16:55:53 -0400

    Add login page and server script

---
+++ server/login.js
@@ -0,0 +1,73 @@
+// login.js
+
+import bcrypt from 'bcrypt';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import jsonwebtoken from 'jsonwebtoken';
+import db from './db.js';
+
+function genJWT(user) {
+   const payload = { id: user._id };
+   const jwtToken = jsonwebtoken.sign(payload, 'secret-key', { expiresIn: '1h' });
+    user.jwtToken = jwtToken;
+    return { success: true, token: jwtToken };
+}
+
+async function loginUser(username, password) {
+   try {
+      const userSchema = new db.Schema({
+         username: { type: String, required: true },
+         password: { type: String, required: true },
+         email: { type: String, required: true }
+       });
+      const User = db.model('User', userSchema);
+
+      const user = await User.findOne({ username: username }).select('+password') || await User.findOne({ email: username }).select('+password');
+      console.log('Retrieved User:', user);
+      if (!user) {
+      throw new Error('Username does not exist');
+     }
+     const isPasswordValid = bcrypt.compareSync(password, user.password);
+     if (!isPasswordValid) {
+       throw new Error('Password is incorrect');
+     }
+     return genJWT(user);
+   } catch (error) {
+     console.error('Error logging in user: ', error);
+     throw error;
+   }
+ }
+
+function login() {
+   const loginApp = express();
+   loginApp.use(bodyParser.json());
+   loginApp.use(bodyParser.urlencoded({ extended: true }));
+   loginApp.use(cors());
+   
+   
+   loginApp.post('/login', async (req, res) => {
+     try {
+      const { userOrEmail, password } = req.body;
+      const userId = await loginUser(userOrEmail, password);
+
+      const jwtToken = userId.token;
+      res.cookie('jwt', jwtToken, { httpOnly: true, secure: true });
+      res.status(201).json({ success: userId.success, message: 'Login Successful' });
+      } catch (errr) {
+      console.error('Error while logging in: ', errr);
+      res.status(500).json({ success: false, message: errr.toString()});
+      }
+   });
+   
+   const port = process.env.LOGPORT || 3000;
+   
+   loginApp.listen(port, () => {
+     console.log(`login listening on port ${port}`);
+   });
+  }
+
+export default  login;
+
+
+   
